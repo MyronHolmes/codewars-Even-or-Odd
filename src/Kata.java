83c0af2ec05ebb36cdf4c1e0841f807988706c66
Myron Holmes <1dequanh@gmail.com>
2023-10-24 17:03:49 -0400

    added instructions

--- src/Kata.java
+++ src/Kata.java
@@ -1,4 +1,6 @@
 public class Kata {
+
+//    Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
     public static String evenOrOdd(int number) {
         String answer = null;
         if(number % 2 == 0){
